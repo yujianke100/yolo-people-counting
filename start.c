951bf6a7f6710b176e6bf70f17134baa6082b52d
yujianke100 <35999482+yujianke100@users.noreply.github.com>
2018-10-17 15:12:48 +0800

    添加了对IP摄像头的支持

--- start.c
+++ start.c
@@ -1,4 +1,16 @@
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 void main(){
-	system("./darknet detector yujianke cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights -c 1 -w 640 -h 480");
+	char input[10];
+	int comp;
+	printf("请选择USB摄像头或网络摄像头(默认USB)\n请输入usb或ip:\n");
+	scanf("%s",input);
+	comp=strcmp(input,"ip");
+	if(comp==0)
+	    system("./darknet detector yujianke cfg/coco.data cfg/yolov3.cfg yolov3.weights rtsp://admin:aa123456789@10.20.4.120/h264/ch1/sub/av_stream -w 640 -h 480");
+
+	else
+	    system("./darknet detector yujianke cfg/coco.data cfg/yolov3-tiny.cfg yolov3-tiny.weights -c 1 -w 640 -h 480");
+	    
 }
