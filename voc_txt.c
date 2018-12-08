04e1a29640abd3a9cba10b27fe1f247d8f605277
Shinnshi <35999482+yujianke100@users.noreply.github.com>
2018-12-08 17:17:15 +0800

    Update voc_txt.c

--- voc_txt.c
+++ voc_txt.c
@@ -1,9 +1,10 @@
 #include <cstdlib>
 int main()
 {   
-   system("ls ./VOCdevkit/VOC2007/JPEGImages *.jpg > ./trainval.txt");
-   system("ls ./VOCdevkit/VOC2007/JPEGImages *0.jpg *5.jpg > ./val.txt");
-   system("ls ./VOCdevkit/VOC2007/JPEGImages *[^05].jpg > ./train.txt");
+   system("ls ./VOCdevkit/VOC2007/JPEGImages/*[^1].jpg > ./trainval.txt");
+   system("ls ./VOCdevkit/VOC2007/JPEGImages/*0.jpg ./VOCdevkit/VOC2007/JPEGImages/*5.jpg > ./val.txt");
+   system("ls ./VOCdevkit/VOC2007/JPEGImages/*[^0^1^5].jpg > ./train.txt");
+   system("ls ./VOCdevkit/VOC2007/JPEGImages/*1.jpg > ./test.txt");
    return 0;
 }
 //ls 18*8.jpg 0*.jpg >test.txt
